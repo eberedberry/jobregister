33482f20e657f295db8892a7393b5370ab23f269
eberedberry <memepeace90@yahoo.com>
2022-09-14 16:05:30 +0100

    allowed host

--- config/settings.py
+++ config/settings.py
@@ -188,7 +188,7 @@ class Staging(Common):
     
     # Security
     DEBUG = False
-    ALLOWED_HOSTS = "registerjob.herokuapp.com"
+    ALLOWED_HOSTS = ["registerjob.herokuapp.com"]
     SESSION_COOKIE_SECURE = values.BooleanValue(True)
     SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
     SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
